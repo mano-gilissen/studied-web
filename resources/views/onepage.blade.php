9f57b74547554dcb7fb97b8e1cb9e095b7031f90
BasJennissen <b.jennissen@studied.nl>
2020-08-15 17:54:01 +0200

    onepage aangemaakt

---
+++ resources/views/onepage.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Studied</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color:               aqua;
+        }
+
+        .text-standaard {
+            font-family:                    'Nunito', sans-serif;
+            font-weight:                    600;
+            font-size:                      50px;
+            color:                          #FF00FF;
+            padding:                        160px;
+        }
+
+    </style>
+</head>
+<body>
+
+    <div class="text-standaard">
+        hoi ik ben bas welkom op mijn website
+    </div>
+
+</body>
+</html>
