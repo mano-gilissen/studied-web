5b75be32655ed594fcaa52e8fb5e5f96e496cd59
BasJennissen <b.jennissen@studied.nl>
2020-08-15 17:35:39 +0200

    Titel veranderd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Studied
                 </div>
 
                 <div class="links">
