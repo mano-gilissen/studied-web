6188a840e4d4a1c041aaa8186b479fcec0ba0ec3
mano-gilissen <A7c20m1095>
2021-04-12 18:33:56 +0200

    Test

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,23 @@
+@extends('app')
+
+
+
+@section('css')
+
+    {{HTML::style('css/landing.css')}}
+
+@endsection
+
+
+
+@section('content')
+
+    <div class="wrap">
+
+        <div class="dot">
+
+        </div>
+
+    </div>
+
+@endsection
