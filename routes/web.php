94f267a1a89b4ec113e4d38eae05b148f5a0ce88
mano-gilissen <A7c20m1095>
2021-04-13 19:45:00 +0200

    Auth Landing Structure

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,17 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Auth::routes(['register' => false, 'verify' => false]);
+Auth::routes();
 
 
 
-Route::get('/',                             'HomeController@landing')->name('landing');
+Route::get('/',                                 'HomeController@landing')->name('landing');
 
-Route::get('/home',                         'HomeController@home')->name('home')->middleware('auth');
+Route::get('/home',                             'HomeController@home')->name('home')->middleware('auth');
+/*
+Route::get('password/email',                    'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
+Route::post('password/email',                   'Auth\ForgotPasswordController@sendResetLinkEmail');
+
+Route::get('password/reset/{token}',            'Auth\ResetPasswordController@showResetForm')->name('password.request');
+Route::post('password/reset',                   'Auth\ResetPasswordController@postReset')->name('password.reset');
+*/
