4976c1f194c6cd760b5f6c1d8a95dfe1dfdd9fc9
mano-gilissen <A7c20m1095>
2021-04-13 20:07:17 +0200

    Password reset

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,20 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Auth::routes();
+Auth::routes(['register' => false, 'verify' => false]);
+
+Route::get('password/email',                    'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
+
+Route::post('password/email',                   'Auth\ForgotPasswordController@sendResetLinkEmail');
+
+Route::get('password/reset/{token}',            'Auth\ResetPasswordController@showResetForm')->name('password.request');
+
+Route::post('password/reset',                   'Auth\ResetPasswordController@postReset')->name('password.reset');
+
+
 
 
 
 Route::get('/',                                 'HomeController@landing')->name('landing');
 
 Route::get('/home',                             'HomeController@home')->name('home')->middleware('auth');
-/*
-Route::get('password/email',                    'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
-Route::post('password/email',                   'Auth\ForgotPasswordController@sendResetLinkEmail');
-
-Route::get('password/reset/{token}',            'Auth\ResetPasswordController@showResetForm')->name('password.request');
-Route::post('password/reset',                   'Auth\ResetPasswordController@postReset')->name('password.reset');
-*/
