eede3c4d35ec07d3dd6b2a0c9fcac2f46dbab1c3
mano-gilissen <A7c20m1095>
2021-05-16 22:25:08 +0200

    Authenticate middleware redirect to dutch login url

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,21 +1,28 @@
 <?php
 
+
+
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
-{
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
+
+
+class Authenticate extends Middleware {
+
+
+
+    protected function redirectTo($request) {
+
         if (! $request->expectsJson()) {
-            return route('login');
+
+            /** Redirect route when not logged in */
+
+            return route('login.view');
         }
+
     }
+
+
+
 }
