d3b6b7c4ba5801084442212fb7b84f2b48933b0f
mano-gilissen <A7c20m1095>
2021-06-18 18:08:10 +0200

    Added generic form styling Added generic form styling test

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Er is geen account met dit email adres.',
+    'throttle' => 'Te veel inlog pogingen, probeer het later opnieuw.',
 
 ];
