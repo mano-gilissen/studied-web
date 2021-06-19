ada4a8665a2fc42e79c2c7f89c7d878b12dbda35
mano-gilissen <A7c20m1095>
2021-06-19 18:56:09 +0200

    Autocomplete module CSS Structure

--- resources/views/form/box-input.blade.php
+++ resources/views/form/box-input.blade.php
@@ -2,23 +2,21 @@
 
     <div class="box-input @error($id) invalid @enderror" @isset($id_box) id="{{ $id_box }}" @endisset >
 
-        <div class="autocomplete">
+        <input
+            id                                          = "{{ $id }}"
+            type                                        = "{{ $type ?? 'text' }}"
+            name                                        = "{{ $id }}"
+            placeholder                                 = "{{ $placeholder ?? '' }}"
+            value                                       = "{{ $value ?? '' }}"
+            autocomplete                                = "{{ $autocomplete ?? 'off' }}"
 
-            <input
-                id                                          = "{{ $id }}"
-                type                                        = "{{ $type ?? 'text' }}"
-                name                                        = "{{ $id }}"
-                placeholder                                 = "{{ $placeholder ?? '' }}"
-                value                                       = "{{ $value ?? '' }}"
-                autocomplete                                = "{{ $autocomplete ?? 'off' }}"
+            @isset($required) required @endisset >
 
-                @isset($required) required @endisset >
-
-            @isset($icon) <img class="icon" src="images/{{ $icon }}"> @endisset
-
-        </div>
+        @isset($icon) <img class="icon" src="images/{{ $icon }}"> @endisset
 
     </div>
+
+    <div class="autocomplete"></div>
 
     @error($id) <div class="input-invalid">{{ $message }}</div> @enderror
 
