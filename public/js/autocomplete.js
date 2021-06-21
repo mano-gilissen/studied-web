ef90a2c36e16e5e7e91c58ed54f9fefee03e3287
mano-gilissen <A7c20m1095>
2021-06-21 12:15:40 +0200

    Autocomplete reject custom input option

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -168,6 +168,7 @@ function autocomplete(input, values, reject_other, show_all) {
 
     function closeAllLists(element) {
 
+        console.log(input.id + "-autocomplete-list");
         var list                                    = document.getElementById(input.id + "-autocomplete-list");
 
         for (var index = 0; index < list.length; index++) {
