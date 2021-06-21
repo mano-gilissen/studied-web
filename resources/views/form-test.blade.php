d3499a872b659c8ec9d157fd46b641157ce7d9be
mano-gilissen <A7c20m1095>
2021-06-21 11:20:54 +0200

    Autocomplete reject custom input option

--- resources/views/form-test.blade.php
+++ resources/views/form-test.blade.php
@@ -6,7 +6,7 @@
 
     <div class="title">{{ __('Gegevens') }}</div>
 
-    @include('form.field-input', ['id' => 'onderwerp', 'tag' => 'Onderwerp', 'placeholder' => 'Vul een onderwerp in', 'required' => true, 'data' => true])
+    @include('form.field-input', ['id' => 'onderwerp', 'tag' => 'Onderwerp', 'placeholder' => 'Vul een onderwerp in', 'required' => true])
 
     @include('form.field-input', ['id' => 'vak', 'tag' => 'Vak', 'icon' => 'search.svg', 'data' => true, 'reject_other' => true])
 
