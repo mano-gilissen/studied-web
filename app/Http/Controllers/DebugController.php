732975d8fa403df8fcca61a49ecb4030c7bc807a
mano-gilissen <A7c20m1095>
2021-07-05 18:16:55 +0200

    Study page template

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -73,7 +73,7 @@ class DebugController extends Controller {
             Key::PAGE_TITLE                                                 => $study->getService->{self::$SERVICE_NAME},
 
             self::$STUDY                                                    => $study,
-            'host'                                                          => [User::find(2)],
+            'host'                                                          => User::find(2),
             'participants'                                                  => [User::find(1), User::find(3)],
             'button_contact_host'                                           => true,
         ]);
