fc4198a81f50aca1c28d52b91ba0d0cb36008840
mano-gilissen <A7c20m1095>
2021-07-08 18:48:07 +0200

    Study page template

--- app/Http/Traits/ReportTrait.php
+++ app/Http/Traits/ReportTrait.php
@@ -26,9 +26,13 @@ trait ReportTrait {
 
         $report_subjects                            = $report->getReport_Subjects();
 
+        return $report_subjects->first()->duration;
+
+        $duration_total                             = (float) 0.0;
+
         foreach($report_subjects as $report_subject) {
 
-            return $report_subject->duration;
+            $duration_total                         = (float) $report_subject->duration;
 
         }
 
