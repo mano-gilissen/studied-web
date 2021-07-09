797bbfa3bbbd85d17e74002d902990573104dbe3
mano-gilissen <A7c20m1095>
2021-07-09 10:24:44 +0200

    Study page template

---
+++ public/js/events.js
@@ -0,0 +1,38 @@
+
+
+
+const BUTTON_MENU                               = '#button-menu';
+
+const OBJECT_WRAP                               = '#wrap';
+const OBJECT_MENU                               = '#menu';
+
+var menu_open                                   = false;
+
+
+
+$(function(){
+
+
+
+    $(BUTTON_MENU).click(function() {
+
+        console.log("a");
+
+        menu_open                               = !menu_open;
+
+        if (this.menu_open) {
+
+            $(OBJECT_WRAP).hide();
+            $(OBJECT_MENU).show();
+
+        } else {
+
+            $(OBJECT_WRAP).show();
+            $(OBJECT_MENU).hide();
+
+        }
+    });
+
+
+
+});
