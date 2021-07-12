c54903c611b8fc24cf510f6e87b1853e2d54229f
mano-gilissen <A7c20m1095>
2021-07-12 11:38:19 +0200

    Study page template

--- app/Http/Support/Views.php
+++ app/Http/Support/Views.php
@@ -14,6 +14,7 @@ class Views {
     const LAB                                   = "lab";
     const LANDING                               = "landing";
     const STUDY                                 = "template.study";
+    const PROFILE                               = "template.profile";
     const FORM_TEST                             = "form-test";
 
 
