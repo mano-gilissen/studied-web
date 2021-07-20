62060b71dfc48feb3b889a2abf6d1b14495a93d6
mano-gilissen <A7c20m1095>
2021-07-20 19:40:20 +0200

    f

--- public/js/list.js
+++ public/js/list.js
@@ -4,8 +4,7 @@
 const OBJECT_LIST                               = '#list';
 const CLASS_HEADER                              = '.header';
 
-var data_sort_data                              = [];
-var test                                        = 'aaa';
+var data                                        = [];
 
 
 
@@ -15,19 +14,19 @@ $(function(){
 
     function load() {
 
+        Console.log(data);
+
         $(OBJECT_LIST).load('/load/list/' + data_type, {
 
-            data_sort:                          this.test
+            data_sort:                          this.data
 
         });
     }
 
     function sort(id) {
 
-        this.data_sort_data['id']               = id;
-        this.data_sort_data['mode']             = 'asc';
-
-        this.test                               = id;
+        this.data['id']               = id;
+        this.data['mode']             = 'asc';
 
         load();
     }
