be47efda9a1e994ceb1063f91f467c7761fd991f
mano-gilissen <A7c20m1095>
2021-07-20 19:12:40 +0200

    f

--- app/Http/Support/Table.php
+++ app/Http/Support/Table.php
@@ -38,7 +38,7 @@ class Table {
 
     public static function load($controller, $objects, $sort) {
 
-        dd($sort);
+        dd(json_encode($sort));
 
         $columns                                            = $controller->list_columns();
         $spacing                                            = self::spacing($columns);
