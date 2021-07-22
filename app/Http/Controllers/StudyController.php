afb1ae9c5f879ea2f9c2adbd9785c7730eed801f
mano-gilissen <A7c20m1095>
2021-07-22 15:54:10 +0200

    f

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -112,9 +112,9 @@ class StudyController extends Controller {
                     Table::column(self::$COLUMN_DATE, 'Datum', 3, true, $sort, true),
                     Table::column(self::$COLUMN_STUDENT, 'Leerling', 4, true, $sort),
                     Table::column(self::$COLUMN_HOST, 'Student', 4, true, $sort),
-                    Table::column(self::$COLUMN_SERVICE, 'Dienst', 2, true, $sort),
-                    Table::column(self::$COLUMN_SUBJECT, 'Onderwerp', 2, false, $sort),
-                    Table::column(self::$COLUMN_LOCATION, 'Locatie', 3, false, $sort),
+                    Table::column(self::$COLUMN_SERVICE, 'Dienst', 3, true, $sort),
+                    Table::column(self::$COLUMN_SUBJECT, 'Onderwerp', 3, false, $sort),
+                    Table::column(self::$COLUMN_LOCATION, 'Locatie', 4, false, $sort),
                     Table::column(self::$COLUMN_TIME, 'Tijdstip', 3, true, $sort),
                     Table::column(self::$COLUMN_STATUS, 'Status', 3, true, $sort, true)
                 );
