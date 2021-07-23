66279fb480c52d6004129ad2b47be345c4ab590a
mano-gilissen <A7c20m1095>
2021-07-24 01:54:31 +0200

    Profile

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/lab';
+    public const HOME = '/lessen';
 
     /**
      * Define your route model bindings, pattern filters, etc.
