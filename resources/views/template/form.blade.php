c2a03df27321866abc82ac2f2467c06b921f151c
mano-gilissen <A7c20m1095>
2021-07-27 12:00:56 +0200

    Study form

--- resources/views/template/form.blade.php
+++ resources/views/template/form.blade.php
@@ -16,7 +16,7 @@
 
         <div class="wrap">
 
-            @include('block.header-navigation', ['page_back', true])
+            @include('block.header-navigation', ['page_back', 'true'])
 
         </div>
 
