96e3c68330b2d518e214f372ccd2c62ba33bdf4a
mano-gilissen <A7c20m1095>
2021-07-28 20:04:36 +0200

    Study plan form

--- app/Http/Traits/StudyTrait.php
+++ app/Http/Traits/StudyTrait.php
@@ -35,7 +35,7 @@ trait StudyTrait {
 
         $study->{Model::$STUDY_DATE}                    = date_create_from_format('y-m-d', $data[Model::$STUDY_DATE]);
 
-        dd($study);
+        dd($data[Model::$STUDY_DATE]);
 /*
         $study->{Model::$STUDY_STATUS}                  = ;
         $study->{Model::$STUDY_HOST_USER}               = ;
