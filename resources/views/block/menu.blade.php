f7df800438e79dc249bf98b0a0e8a88ecb8cf507
mano-gilissen <A7c20m1095>
2021-07-28 12:29:08 +0200

    Study plan form

--- resources/views/block/menu.blade.php
+++ resources/views/block/menu.blade.php
@@ -1,71 +1,83 @@
 <div id="menu" style="display: none">
 
-    @switch(Auth::user()->role)
+    <div>
 
-        @case(\App\Http\Traits\RoleTrait::$ID_ADMINISTRATOR)
+        @switch(Auth::user()->role)
 
-        @case(\App\Http\Traits\RoleTrait::$ID_BOARD)
+            @case(\App\Http\Traits\RoleTrait::$ID_ADMINISTRATOR)
 
-            @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
+            @case(\App\Http\Traits\RoleTrait::$ID_BOARD)
 
-            @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
+                @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
 
-            @include('block.menu-item', ['label' => 'Medewerkers'])
+                @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
 
-            @include('block.menu-item', ['label' => 'Leerlingen'])
+                @include('block.menu-item', ['label' => 'Medewerkers'])
 
-            @include('block.menu-item', ['label' => 'Klanten'])
+                @include('block.menu-item', ['label' => 'Leerlingen'])
 
-            @include('block.menu-item', ['label' => 'Locatieagenda'])
+                @include('block.menu-item', ['label' => 'Klanten'])
 
-            @break
+                @include('block.menu-item', ['label' => 'Locatieagenda'])
 
-        @case(\App\Http\Traits\RoleTrait::$ID_MANAGEMENT)
+                @break
 
-            @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
+            @case(\App\Http\Traits\RoleTrait::$ID_MANAGEMENT)
 
-            @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
+                @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
 
-            @include('block.menu-item', ['label' => 'Medewerkers'])
+                @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
 
-            @include('block.menu-item', ['label' => 'Leerlingen'])
+                @include('block.menu-item', ['label' => 'Medewerkers'])
 
-            @include('block.menu-item', ['label' => 'Klanten'])
+                @include('block.menu-item', ['label' => 'Leerlingen'])
 
-            @include('block.menu-item', ['label' => 'Locatieagenda'])
+                @include('block.menu-item', ['label' => 'Klanten'])
 
-            @break
+                @include('block.menu-item', ['label' => 'Locatieagenda'])
 
-        @case(\App\Http\Traits\RoleTrait::$ID_EMPLOYEE)
+                @break
 
-            @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
+            @case(\App\Http\Traits\RoleTrait::$ID_EMPLOYEE)
 
-            @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])])
+                @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
 
-            @include('block.menu-item', ['label' => 'Leerlingen'])
+                @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])])
 
-            @include('block.menu-item', ['label' => 'Locatieagenda'])
+                @include('block.menu-item', ['label' => 'Leerlingen'])
 
-            @break
+                @include('block.menu-item', ['label' => 'Locatieagenda'])
 
-        @case(\App\Http\Traits\RoleTrait::$ID_STUDENT)
+                @break
 
-            @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
+            @case(\App\Http\Traits\RoleTrait::$ID_STUDENT)
 
-            @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
+                @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
 
-            @break
+                @include('block.menu-item', ['label' => 'Lessen', 'route' => route('study.list')])
 
-        @case(\App\Http\Traits\RoleTrait::$ID_CUSTOMER)
+                @break
 
-            @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
+            @case(\App\Http\Traits\RoleTrait::$ID_CUSTOMER)
 
-            @include('block.menu-item', ['label' => 'Lessen', 'route' => route('person.view', Auth::user()->getPerson->slug)])
+                @include('block.menu-item', ['label' => 'Mijn profiel', 'route' => route('person.self')])
 
-            @include('block.menu-item', ['label' => 'Leerlingen'])
+                @include('block.menu-item', ['label' => 'Lessen', 'route' => route('person.view', Auth::user()->getPerson->slug)])
 
-            @break
+                @include('block.menu-item', ['label' => 'Leerlingen'])
 
-    @endswitch
+                @break
+
+        @endswitch
+
+    </div>
+
+    <div id="menu-actions">
+
+        <img id="button-logout" src="/images/logout.svg"/>
+
+        <img id="button-settings" src="/images/settings.svg"/>
+
+    </div>
 
 </div>
