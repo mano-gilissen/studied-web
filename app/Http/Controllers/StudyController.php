feb2b36c600d77ac4e17e22254388d5c0a8c33e6
mano-gilissen <A7c20m1095>
2021-07-28 15:21:01 +0200

    Study plan form

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -75,7 +75,10 @@ class StudyController extends Controller {
         $objects_location                                   = Location::all();
         $objects_subject                                    = Subject::all();
 
-        $ac_data_location                                   = $objects_location->pluck(Model::$LOCATION_NAME)->toArray();
+        $ac_data_location                                   = $objects_location->pluck(Model::$LOCATION_NAME, Model::$BASE_ID)->toArray();
+
+        dd($ac_data_location);
+
         $ac_data_subject                                    = $objects_subject->pluck(Model::$SUBJECT_DESCRIPTION_SHORT)->toArray();
 
         $ac_additional_subject                              = $objects_subject->pluck(Model::$SUBJECT_CODE)->toArray();
