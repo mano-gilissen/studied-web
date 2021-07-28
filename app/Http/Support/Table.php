f2903b96127556476d32382db0443b11f571533c
mano-gilissen <A7c20m1095>
2021-07-28 20:22:17 +0200

    Study plan form

--- app/Http/Support/Table.php
+++ app/Http/Support/Table.php
@@ -48,6 +48,9 @@ class Table {
         $sort                                               = $request->input(Table::DATA_SORT, null);
         $filter                                             = $request->input(Table::DATA_FILTER, null);
 */
+
+        $sort = null;
+
         $columns                                            = $controller->list_columns($sort);
         $spacing                                            = self::spacing($columns);
         $objects                                            = self::objects($controller, $sort);
