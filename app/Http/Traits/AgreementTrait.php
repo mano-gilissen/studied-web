7a866515babb0581c922416ecb03ead9aaef2d6e
mano-gilissen <A7c20m1095>
2021-07-30 22:50:45 +0200

    Study plan form

--- app/Http/Traits/AgreementTrait.php
+++ app/Http/Traits/AgreementTrait.php
@@ -15,9 +15,9 @@ trait AgreementTrait {
 
     public static function getDescription($agreement, $for_host = false) {
 
-        return $agreement->getSubject->{Model::$SUBJECT_DESCRIPTION_SUBJECT} . ' met '
+        return $agreement->getSubject->{Model::$SUBJECT_DESCRIPTION_SUBJECT} . ' met <span style="font-weight: 600">'
             . ($for_host ? $agreement->getStudent->getPerson->{Model::$PERSON_FIRST_NAME} : $agreement->getEmployee->getPerson->{Model::$PERSON_FIRST_NAME})
-            . ',<br> actief tot ' . Format::datetime($agreement->{Model::$AGREEMENT_END}, Format::$DATETIME_AGREEMENT) . '.';
+            . '</span>,<br> actief tot ' . Format::datetime($agreement->{Model::$AGREEMENT_END}, Format::$DATETIME_AGREEMENT) . '.';
     }
 
 
