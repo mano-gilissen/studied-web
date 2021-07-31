547c9e4475e46a4790cd152d464f4de0a16bda36
mano-gilissen <A7c20m1095>
2021-07-31 19:50:49 +0200

    Study plan form

--- public/js/form.js
+++ public/js/form.js
@@ -39,7 +39,7 @@ $(function(){
 
 function agreements_load(host) {
 
-    $('#agreements').load('/load/agreements', {
+    $('.agreements').load('/load/agreements', {
 
         user:                               host
 
