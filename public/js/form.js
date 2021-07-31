9108362b84123f46b79b26e4c36e1f9b888b37f0
mano-gilissen <A7c20m1095>
2021-07-31 13:59:59 +0200

    Study plan form

--- public/js/form.js
+++ public/js/form.js
@@ -52,7 +52,7 @@ function agreements_set_active(index) {
     agreements                                  = $(CLASS_AGREEMENT);
 
     agreements                                  .removeClass('active');
-    agreements[agreements_index_active]         .addClass('active');
+    agreements.index(agreements_index_active)   .addClass('active');
 }
 
 
