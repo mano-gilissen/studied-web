e373e0da229d7b1059a66f65dfd7753aa6558f49
mano-gilissen <A7c20m1095>
2021-08-02 22:23:19 +0200

    Reporting

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -54,8 +54,6 @@ class StudyController extends Controller {
 
         $study                                              = Study::where(Model::$BASE_KEY, $key)->firstOrFail();
 
-        return (StudyTrait::hasStarted($study) ? 'a' : 'b') . '<br>date: ' . $study->date . '<br>now: ' . date('Y-m-d H:i:s', time()) . '<br>start: ' . $study->start . '<br>end: ' . $study->end;
-
         return view(Views::STUDY, [
 
             Key::PAGE_TITLE                                 => $study->getService->{Model::$SERVICE_NAME},
