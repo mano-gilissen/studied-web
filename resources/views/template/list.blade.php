0a29209d7a67bf0d22c28beb159af929d31504c6
mano-gilissen <A7c20m1095>
2021-08-04 11:25:40 +0200

    Reporting

--- resources/views/template/list.blade.php
+++ resources/views/template/list.blade.php
@@ -16,8 +16,8 @@
 
         var data_type                       = '{{ $data_type }}';
 
-        data_sort                           = JSON.parse('{!! $data_sort !!}');
-        data_filter                         = JSON.parse('{!! $data_filter !!}');
+        var data_sort                       = JSON.parse('{!! $data_sort !!}');
+        var data_filter                     = JSON.parse('{!! $data_filter !!}');
 
     </script>
 
