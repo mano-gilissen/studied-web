6483d338f3a7d3e906a4f1d224fdfd030832b7bd
mano-gilissen <A7c20m1095>
2021-08-04 14:11:26 +0200

    Reporting

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -275,12 +275,12 @@ function autocomplete(input, data, additional, reject_other, show_all, show_alwa
             }
         });
     }
+
+    openList(this);
+
 /*
-
-
     if (show_always) {
 
-        openList(this);
 
     }
 
