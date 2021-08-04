c59da7d43a75152da23e7834efa2c86318bc8081
mano-gilissen <A7c20m1095>
2021-08-04 12:28:01 +0200

    Reporting

--- app/Http/Support/Table.php
+++ app/Http/Support/Table.php
@@ -52,9 +52,6 @@ class Table {
         $sort                                               = $request->input(Table::DATA_SORT, null);
         $filter                                             = $request->input(Table::DATA_FILTER, null);
 
-
-        dd('b');
-
         $columns                                            = $controller->list_columns($sort);
 
         dd('a');
