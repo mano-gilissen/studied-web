8d636b93722aa9423d5ba333fad13e780c1141f2
mano-gilissen <A7c20m1095>
2021-08-07 00:27:49 +0200

    Table module restructure

--- resources/views/block/study-participants-college.blade.php
+++ resources/views/block/study-participants-college.blade.php
@@ -6,7 +6,7 @@
 
         @foreach(\App\Http\Traits\StudyTrait::getParticipants_Person($study) as $person)
 
-            @include('block.person', ['person' => $person])
+            @include('block.person', ['person' => $person, 'size' => 'grid'])
 
         @endforeach
 
