31f176bc31a43dc06609cb45bd87a23463011a87
mano-gilissen <A7c20m1095>
2021-08-14 21:57:44 +0200

    Les rapporteren

--- public/js/form.js
+++ public/js/form.js
@@ -244,11 +244,8 @@ function subjects_load() {
     let start                                       = $("#start").val();
     let end                                         = $("#end").val();
 
-    var time_available                              = ((end.substr(0, 2) * 60) + (1 * end.substr(3, 2))) - ((start.substr(0, 1) * 60) + (1 * start.substr(3, 2)));
+    var time_available                              = ((end.substr(0, 2) * 60) + (1 * end.substr(3, 2))) - ((start.substr(0, 2) * 60) + (1 * start.substr(3, 2)));
 
-    console.log(end.substr(0, 2) * 60);
-    console.log(1 * end.substr(3, 2));
-    console.log((end.substr(0, 2) * 60) + (1 * end.substr(3, 2)));
     console.log(time_available);
 }
 
