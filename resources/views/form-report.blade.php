9b24e979d2b33d6fd050664f3bec0dda3af3561f
mano-gilissen <A7c20m1095>
2021-08-14 13:31:52 +0200

    Les rapporteren

--- resources/views/form-report.blade.php
+++ resources/views/form-report.blade.php
@@ -14,7 +14,7 @@
 
     <div class="title">{{ __('Wat is er behandeld?') }}</div>
 
-    @include('form.field-report-subject', ['id' => 'primary', 'available' => 75, 'locked' => false])
+    @include('form.field-report-subject', ['id' => 'primary', 'available' => 75, 'locked' => true, 'set_id' => 3])
 
 
 
