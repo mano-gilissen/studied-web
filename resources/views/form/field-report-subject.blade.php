6fdd2111b7df61649395754df6236c67c9297d0f
mano-gilissen <A7c20m1095>
2021-08-14 13:11:52 +0200

    Les rapporteren

--- resources/views/form/field-report-subject.blade.php
+++ resources/views/form/field-report-subject.blade.php
@@ -1,4 +1,4 @@
-@include('form.field-input', ['id' => 'subject_' . $id, 'tag' => 'Activiteit', 'icon' => 'close.svg', 'placeholder' => 'Zoek een activiteit', 'required' => true, 'data' => true, 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'locked' => $locked, 'set_id' => $set_id])
+@include('form.field-input', ['id' => 'subject_' . $id, 'tag' => 'Activiteit', 'icon' => 'close.svg', 'placeholder' => 'Zoek een activiteit', 'required' => true, 'data' => true, 'ac_data' => 'subject', 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'locked' => $locked, 'set_id' => $set_id])
 
 @include('form.field-select-duration', ['id' => $id, 'available' => $available])
 
