04253e4681c7d12aa492bd848e0bb5091f8c624d
mano-gilissen <A7c20m1095>
2021-08-14 17:15:24 +0200

    Les rapporteren

--- public/js/form.js
+++ public/js/form.js
@@ -37,7 +37,7 @@ $(function(){
 
 
 
-    $(OBJECT_FORM).on('change', ':select', function() {
+    $(OBJECT_FORM).on('change', 'select', function() {
 
         console.log('b');
         select_trigger($(this));
