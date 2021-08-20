e2b07df82270f71214f85649af42218333bd6691
mano-gilissen <A7c20m1095>
2021-08-20 11:49:36 +0200

    Les rapporteren

--- resources/views/form-report.blade.php
+++ resources/views/form-report.blade.php
@@ -22,7 +22,7 @@
 
     @if($errors->any())
 
-        <div class="block-note error">Los alle problemen op en probeer het opnieuw.</div>
+        <div class="block-note error">Vul de juiste gegevens in en probeer het opnieuw.</div>
 
         <div class="seperator"></div>
 
