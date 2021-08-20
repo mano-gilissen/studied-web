eea34f2496b2b3810f957689abbc8330c4c93bb8
mano-gilissen <A7c20m1095>
2021-08-20 10:21:59 +0200

    Les rapporteren

--- resources/views/form-report.blade.php
+++ resources/views/form-report.blade.php
@@ -38,6 +38,8 @@
 
         @if(\App\Http\Traits\StudyTrait::hasGroupReporting($study))
 
+            <div class="seperator"></div>
+
             <div class="block-note">Dit het rapport voor <span style="font-weight: 400">{{ \App\Http\Traits\PersonTrait::getFullName($user->getPerson) }}</span></div>
 
             <div class="seperator"></div>
