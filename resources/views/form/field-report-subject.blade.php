b5a431a797cfe8c0ed28db934a088b71a1794e5a
mano-gilissen <A7c20m1095>
2021-08-22 22:26:06 +0200

    REDO SUBJECT SYSTEM SEPERATION SUBJECTS AND LEVELS LINK STUDY TO AGREEMENT

--- resources/views/form/field-report-subject.blade.php
+++ resources/views/form/field-report-subject.blade.php
@@ -1,5 +1,7 @@
 @php $id_user = 'user_' . $user->id . '_'; @endphp
 
+@php echo($agreement) @endphp
+
 
 
 @include('form.field-input', ['id' => $id_user . 'subject_' . $id, 'tag' => 'Activiteit', 'icon' => $primary ? 'fix.svg' : 'search.svg', 'placeholder' => 'Zoek een activiteit', 'required' => true, 'data' => true, 'additional' => true, 'ac_data' => 'subject_' . ($primary ? 'primary' : 'secondary'), 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'locked' => $primary, 'set_id' => $primary ? $study->subject_defined : -1])
@@ -10,7 +12,7 @@
 
 
 
-@if ($primary)
+@if (false)
 
     @include('form.field-hidden', ['id' => $id_user . 'subject_' . $id . '_agreement', 'value' => $agreement->id])
 
