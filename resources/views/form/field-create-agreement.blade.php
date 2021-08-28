dad94de42adc0858d3a664bd0b6fe16093b6d20c
mano-gilissen <A7c20m1095>
2021-08-28 10:59:13 +0200

    agreements on profile

--- resources/views/form/field-create-agreement.blade.php
+++ resources/views/form/field-create-agreement.blade.php
@@ -2,17 +2,17 @@
 
 @include('form.field-input', ['id' => 'student_' . $id, 'tag' => 'Leerling', 'icon' => 'fix.svg', 'required' => true, 'data' => true, 'additional' => true, 'ac_data' => 'student', 'uses_id' => true, 'locked' => true, 'set_id' => $student->id])
 
-@include('form.field-input', ['id' => 'employee_' . $id, 'tag' => 'Student-docent', 'icon' => 'search.svg', 'required' => true, 'data' => true, 'additional' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'employee', 'uses_id' => true, 'placeholder' => 'Kies de Student-docent'])
+@include('form.field-input', ['id' => 'employee_' . $id, 'tag' => 'Student-docent', 'icon' => 'search.svg', 'required' => true, 'data' => true, 'additional' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'employee', 'uses_id' => true])
 
 <div class="field">
 
     <div class="name">Vak</div>
 
-    @include('form.box-input', ['id' => 'subject_' . $id, 'required' => true, 'data' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'subject', 'uses_id' => true, 'placeholder' => 'Kies een vak', 'size' => 'width-third'])
+    @include('form.box-input', ['id' => 'subject_' . $id, 'required' => true, 'data' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'subject', 'uses_id' => true, 'size' => 'width-third'])
 
     <div class="note width-third">Niveau</div>
 
-    @include('form.box-input', ['id' => 'level_' . $id, 'required' => true, 'data' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'level', 'uses_id' => true, 'placeholder' => 'Kies een niveau', 'size' => 'width-third'])
+    @include('form.box-input', ['id' => 'level_' . $id, 'required' => true, 'data' => true, 'reject_other' => true, 'show_all' => true, 'ac_data' => 'level', 'uses_id' => true, 'size' => 'width-third'])
 
 </div>
 
@@ -28,10 +28,10 @@
 
 </div>
 
-@include('form.field-input', ['id' => 'end_' . $id, 'type' => 'date', 'tag' => 'Geldig tot', 'placeholder' => 'Kies een datum', 'required' => true])
+@include('form.field-input', ['id' => 'end_' . $id, 'type' => 'date', 'tag' => 'Geldig tot', 'required' => true])
 
 @include('form.field-textarea', ['id' => 'remark_' . $id, 'tag' => 'Opmerkingen'])
 
-@include('form.field-input', ['id' => 'replace_' . $id, 'tag' => 'Vervangt', 'icon' => 'search.svg', 'data' => true, 'additional' => true, 'reject_other' => true, 'ac_data' => 'replace', 'uses_id' => true, 'placeholder' => 'Welke vakafspraak wordt vervangen?'])
+@include('form.field-input', ['id' => 'replace_' . $id, 'tag' => 'Vervangt (optioneel)', 'icon' => 'search.svg', 'show_all' => true, 'data' => true, 'additional' => true, 'reject_other' => true, 'ac_data' => 'replace', 'uses_id' => true, 'placeholder' => 'Welke vakafspraak wordt vervangen?'])
 
 <div class="seperator"></div>
