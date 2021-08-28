cf8965f65d2045a2c366986af92eb3afc6c1783e
mano-gilissen <A7c20m1095>
2021-08-28 20:59:29 +0200

    EV2 add remove agreements

--- public/js/evaluation.js
+++ public/js/evaluation.js
@@ -13,7 +13,7 @@ function evaluation_agreement_load(evaluation_id) {
     $('#agreements-wrap').append( jQuery('<div>').load('/load/evaluation/agreement', {
 
         evaluation:                         evaluation_id,
-        id:                                 $('#agreements-wrap div').length + 1
+        id:                                 $('#agreements-wrap').children().length + 1
 
     }));
 }
