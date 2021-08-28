d8f89810b8f258aca77ffb8433d6b5662c1cc2ba
mano-gilissen <A7c20m1095>
2021-08-28 11:14:26 +0200

    agreements on profile

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -208,6 +208,8 @@ class EvaluationController extends Controller {
 
         $data                                                               = $request->all();
 
+        dd(($data);
+
         // self::perform_validate($data);
 
         $evaluation                                                         = Evaluation::findOrFail($data[Key::AUTOCOMPLETE_ID . Model::$EVALUATION]);
