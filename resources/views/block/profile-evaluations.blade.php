7167063d0366dc16ea8e4a7443ef7e705093d857
mano-gilissen <A7c20m1095>
2021-08-28 12:09:01 +0200

    agreements on profile

--- resources/views/block/profile-evaluations.blade.php
+++ resources/views/block/profile-evaluations.blade.php
@@ -16,78 +16,86 @@
 
     @endif
 
-    <div id="list">
+    @php $evaluations = \App\Http\Traits\UserTrait::getEvaluations($person->getUser); @endphp
 
-        <div id="headers" style="grid-template-columns: 1fr 1fr 1fr 1fr 48px">
+    @if(count($evaluations) > 0)
 
-            <div class="header no_sort no_filter">Datum</div>
+        <div id="list">
 
-            <div class="header no_sort no_filter">Ingepland door</div>
+            <div id="headers" style="grid-template-columns: 1fr 1fr 1fr 1fr 48px">
 
-            <div class="header no_sort no_filter">Locatie</div>
+                <div class="header no_sort no_filter">Datum</div>
 
-            <div class="header no_sort no_filter">Betreft</div>
+                <div class="header no_sort no_filter">Ingepland door</div>
+
+                <div class="header no_sort no_filter">Locatie</div>
+
+                <div class="header no_sort no_filter">Betreft</div>
+
+            </div>
+
+            <div id="items">
+
+                @empty($evaluations)
+
+                    <div class="no-items">{{ $person->first_name }} heeft nog geen gesprekken.</div>
+
+                @else
+
+                    @foreach($evaluations as $evaluation)
+
+                        <div class="item @if($loop->odd) odd @endif" style="grid-template-columns: 1fr 1fr 1fr 1fr 48px" onclick="window.location.href='{{ route('evaluation.view', ['key' => $evaluation->{\App\Http\Support\Model::$BASE_KEY}]) }}'">
+
+                            <div class="attribute">
+
+                                <div style="font-weight: 400">{{ \App\Http\Support\Format::datetime($evaluation->{\App\Http\Support\Model::$EVALUATION_DATETIME}, \App\Http\Support\Format::$DATETIME_LIST) }}</div>
+
+                            </div>
+
+                            <div class="attribute">
+
+                                <div>{{ \App\Http\Traits\PersonTrait::getFullName($evaluation->getHost->getPerson) }}</div>
+
+                            </div>
+
+                            <div class="attribute">
+
+                                <div>{{ $evaluation->{\App\Http\Support\Model::$EVALUATION_LOCATION_TEXT} }}</div>
+
+                            </div>
+
+                            <div class="attribute">
+
+                                <div>{{ \App\Http\Traits\EvaluationTrait::getRegardingText($evaluation->{\App\Http\Support\Model::$EVALUATION_REGARDING}) }}</div>
+
+                            </div>
+
+                            <div class="end">
+
+                                <img class="action" src="{{ $action ?? '/images/chevron-right.svg' }}"/>
+
+                                @if(!\App\Http\Support\Func::has_passed($evaluation->datetime))
+
+                                    <div class="attention"></div>
+
+                                @endif
+
+                            </div>
+
+                        </div>
+
+                    @endforeach
+
+                @endempty
+
+            </div>
 
         </div>
 
-        <div id="items">
+    @else
 
-            @php $evaluations = \App\Http\Traits\UserTrait::getEvaluations($person->getUser); @endphp
+        <div class="block-note">{{ $person->{\App\Http\Support\Model::$PERSON_FIRST_NAME} }} heeft nog geen gesprekken.</div>
 
-            @empty($evaluations)
-
-                <div class="no-items">{{ $person->first_name }} heeft nog geen gesprekken.</div>
-
-            @else
-
-                @foreach($evaluations as $evaluation)
-
-                    <div class="item @if($loop->odd) odd @endif" style="grid-template-columns: 1fr 1fr 1fr 1fr 48px" onclick="window.location.href='{{ route('evaluation.view', ['key' => $evaluation->{\App\Http\Support\Model::$BASE_KEY}]) }}'">
-
-                        <div class="attribute">
-
-                            <div style="font-weight: 400">{{ \App\Http\Support\Format::datetime($evaluation->{\App\Http\Support\Model::$EVALUATION_DATETIME}, \App\Http\Support\Format::$DATETIME_LIST) }}</div>
-
-                        </div>
-
-                        <div class="attribute">
-
-                            <div>{{ \App\Http\Traits\PersonTrait::getFullName($evaluation->getHost->getPerson) }}</div>
-
-                        </div>
-
-                        <div class="attribute">
-
-                            <div>{{ $evaluation->{\App\Http\Support\Model::$EVALUATION_LOCATION_TEXT} }}</div>
-
-                        </div>
-
-                        <div class="attribute">
-
-                            <div>{{ \App\Http\Traits\EvaluationTrait::getRegardingText($evaluation->{\App\Http\Support\Model::$EVALUATION_REGARDING}) }}</div>
-
-                        </div>
-
-                        <div class="end">
-
-                            <img class="action" src="{{ $action ?? '/images/chevron-right.svg' }}"/>
-
-                            @if(!\App\Http\Support\Func::has_passed($evaluation->datetime))
-
-                                <div class="attention"></div>
-
-                            @endif
-
-                        </div>
-
-                    </div>
-
-                @endforeach
-
-            @endempty
-
-        </div>
-
-    </div>
+    @endif
 
 </div>
