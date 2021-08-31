c0a60e04ec1e803ef770ef5cae8e4498061d4fea
mano-gilissen <A7c20m1095>
2021-08-31 21:37:20 +0200

    WEBSITE contact form

--- resources/views/website/article.blade.php
+++ resources/views/website/article.blade.php
@@ -371,7 +371,7 @@ display: block;
     </div>
   </div>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=60ab5c331d3006ab3cefdc9f" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="js/webflow.js" type="text/javascript"></script>
+  <script src="{{ asset('js/webflow.js') }}" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
   <script>
