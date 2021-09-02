51be929244cdc8cbba77a35d5f59e2067ae6c0a2
mano-gilissen <A7c20m1095>
2021-09-02 22:16:37 +0200

    website

--- app/Http/Traits/StudyTrait.php
+++ app/Http/Traits/StudyTrait.php
@@ -187,7 +187,7 @@ trait StudyTrait {
         $study->{Model::$STUDY_END}                             = $data['date'] . ' ' . $data[Model::$STUDY_END] . ':00';
 
         $study->{Model::$STUDY_REMARK}                          = $data[Model::$STUDY_REMARK];
-        $study->{Model::$STUDY_STATUS}                          = $data[Model::$STUDY_STATUS];
+        // $study->{Model::$STUDY_STATUS}                          = $data[Model::$STUDY_STATUS];
 
 
 
