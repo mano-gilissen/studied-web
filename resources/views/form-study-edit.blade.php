f55a991f2ce4066026d6937950e9fe9d11879596
mano-gilissen <A7c20m1095>
2021-09-02 21:21:19 +0200

    website

--- resources/views/form-study-edit.blade.php
+++ resources/views/form-study-edit.blade.php
@@ -10,7 +10,7 @@
 
     @include('form.field-input', ['id' => 'date', 'type' => 'date', 'tag' => 'Datum', 'placeholder' => 'Kies een datum', 'required' => true, 'value' => \App\Http\Support\Format::datetime($study->{\App\Http\Support\Model::$STUDY_START}, \App\Http\Support\Format::$DATETIME_FORM)])
 
-    @include('form.field-select-time', ['set_study' => $study, 'edit' => true])
+    @include('form.field-select-time', ['set_study' => true, 'edit' => true])
 
     @include('form.field-input', ['id' => 'location', 'tag' => 'Locatie', 'icon' => 'search.svg', 'placeholder' => 'Zoek een locatie', 'required' => true, 'data' => true, 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'set_id' => -1])
 
