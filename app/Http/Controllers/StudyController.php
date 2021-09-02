73853671725ec3d04c99344b7b76bd84faf927a9
mano-gilissen <A7c20m1095>
2021-09-02 22:14:39 +0200

    website

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -167,6 +167,8 @@ class StudyController extends Controller {
 
         $data                                                               = $request->all();
 
+        dd($data);
+
         $study                                                              = Study::find($data['_' . Model::$STUDY]);
 
         self::edit_validate($data);
