0883598894c903d84f332cc323dc83dbed8c8af4
mano-gilissen <A7c20m1095>
2021-09-03 14:55:23 +0200

    study edit

--- resources/views/block/study-host-priveles.blade.php
+++ resources/views/block/study-host-priveles.blade.php
@@ -4,4 +4,12 @@
 
     @include('block.person', ['person' => $study->getHost->getPerson, 'size' => 'large'])
 
+    @if(strlen($study->{\App\Http\Support\Model::$STUDY_REMARK}) > 0)
+
+        <div class="comment-tail"></div>
+
+        <div class="comment">"{{ $study->{\App\Http\Support\Model::$STUDY_REMARK} }}"</div>
+
+    @endif
+
 </div>
