5195b7cd1b8b8ab7376771c7459fc22fc5e9f1c7
mano-gilissen <A7c20m1095>
2021-09-03 14:58:51 +0200

    study edit

--- resources/views/block/study-host-priveles.blade.php
+++ resources/views/block/study-host-priveles.blade.php
@@ -6,7 +6,7 @@
 
     @if(strlen($study->{\App\Http\Support\Model::$STUDY_REMARK}) > 0)
 
-        <div class="comment-tail" style="transform: rotate(180deg)"></div>
+        <div class="comment-tail up"></div>
 
         <div class="comment">"{{ $study->{\App\Http\Support\Model::$STUDY_REMARK} }}"</div>
 
