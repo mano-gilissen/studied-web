1d618d22980e52b238b65ce1b9f1f93c438f864b
mano-gilissen <A7c20m1095>
2021-09-06 15:44:02 +0200

    FILTER DATE

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -867,8 +867,8 @@ class StudyController extends Controller {
 
                 case self::$COLUMN_DATE:
 
-                    $after                                  = Format::datetime(strtotime(substr($value, 0, 10)), Format::$DATETIME_LIST);
-                    $before                                 = Format::datetime(strtotime(substr($value, 11, 10)), Format::$DATETIME_LIST);
+                    $after                                  = Format::datetime(DateTime::createFromFormat(Format::$DATABASE_DATE, substr($value, 0, 10)), Format::$DATETIME_LIST);
+                    $before                                 = Format::datetime(DateTime::createFromFormat(Format::$DATABASE_DATE, substr($value, 11, 10)), Format::$DATETIME_LIST);
 
                     $display                                = $after . ' tot ' . $before;
                     break;
