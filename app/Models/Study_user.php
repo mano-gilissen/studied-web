052056500bc821800627154fa33e157070d1bda0
mano-gilissen <A7c20m1095>
2021-09-08 17:49:53 +0200

    TRIAL

--- app/Models/Study_user.php
+++ app/Models/Study_user.php
@@ -5,6 +5,7 @@
 namespace App\Models;
 
 use App\Http\Support\Model;
+use App\Http\Traits\BaseTrait;
 use Illuminate\Database\Eloquent\Model as ModelClass;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -14,6 +15,7 @@ class Study_user extends ModelClass {
 
 
 
+    use BaseTrait;
     use SoftDeletes;
 
 
