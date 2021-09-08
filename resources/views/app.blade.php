957c1a44168c1fe7f95fdb20fc217c10be47cf49
mano-gilissen <A7c20m1095>
2021-09-08 15:33:50 +0200

    MOBILE

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -50,6 +50,8 @@
 
     </div>
 
+    @include('block.mobile')
+
 </body>
 
 
---
+++ resources/views/block/mobile.blade.php
@@ -0,0 +1,11 @@
+<div id="mobile">
+
+    <div id="mobile-wrap">
+
+        <img src="/images_app/logo.svg">
+
+        <div>Gebruik het Studied webportaal door deze op een laptop of tablet te bezoeken!</div>
+
+    </div>
+
+</div>
