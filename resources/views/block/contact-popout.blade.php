8179f545f93e98b0f3a2eed6e7eb70d28bc08c56
mano-gilissen <A7c20m1095>
2021-09-10 02:51:55 +0200

    CONTACT BUTTON

--- resources/views/block/contact-popout.blade.php
+++ resources/views/block/contact-popout.blade.php
@@ -1,6 +1,6 @@
 <div class="button icon">
 
-    <img class="icon" src="/images_app/contact.svg" style="padding: 6px 0 6px" onclick="$(this).parent().toggleClass('clicked')">
+    <img class="icon" src="/images_app/contact.svg" style="height:28px;padding: 6px 0 6px" onclick="$(this).parent().toggleClass('clicked')">
 
     <div class="text" style="padding: 5px 0 7px" onclick="$(this).parent().toggleClass('clicked')">Contacteer {{ $user->getPerson->first_name }}</div>
 
