6370075cc699ff22bebc5c0dd6137d6480c4829e
mano-gilissen <A7c20m1095>
2021-09-10 02:30:43 +0200

    CONTACT BUTTON

--- resources/views/block/contact-popout.blade.php
+++ resources/views/block/contact-popout.blade.php
@@ -1,19 +1,7 @@
 <div class="contact-popout">
 
-    <div class="contact-item">
+    <div class="contact-item">{{ $user->{\App\Http\Support\Model::$USER_EMAIL} }}</div>
 
-        <div class="name">Email</div>
-
-        <div class="value">{{ $user->{\App\Http\Support\Model::$USER_EMAIL} }}</div>
-
-    </div>
-
-    <div class="contact-item">
-
-        <div class="name">Telefoon</div>
-
-        <div class="value">{{ $user->getPerson->{\App\Http\Support\Model::$PERSON_PHONE} }}</div>
-
-    </div>
+    <div class="contact-item">{{ $user->getPerson->{\App\Http\Support\Model::$PERSON_PHONE} }}</div>
 
 </div>
