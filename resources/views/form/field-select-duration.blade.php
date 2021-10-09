9f6a8084fd64a5c36ee491d0949b3ca1d15c32d0
mano-gilissen <A7c20m1095>
2021-10-09 17:52:25 +0200

    REPORT EDIT

--- resources/views/form/field-select-duration.blade.php
+++ resources/views/form/field-select-duration.blade.php
@@ -61,7 +61,7 @@
 
             console.log('{{ $count }}');
 
-            let dot                                         = $("#dots_{{ $id }} .dot")[parseInt('{{ $count }}')];
+            let dot                                         = $("#dots_{{ $id }} .dot").index('{{ $count }}');
 
             report_dot_click(dot);
             report_dot_leave(dot);
