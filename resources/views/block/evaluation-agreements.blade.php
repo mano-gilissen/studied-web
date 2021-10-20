0ef7ca090db4901cbef30db233f59b6088389ec1
mano-gilissen <A7c20m1095>
2021-10-20 15:53:29 +0200

    AGREEMENT IN EVALUATION

--- resources/views/block/evaluation-agreements.blade.php
+++ resources/views/block/evaluation-agreements.blade.php
@@ -1,63 +1,67 @@
-<div class="title">Vakafspraken</div>
+    <div class="agreements">
 
-<div style="display: flex; align-items: center">
+    <div class="title">Vakafspraken</div>
 
-    <img class="button" id="button-previous" src="/images_app/back.svg"/>
+    <div style="display: flex; align-items: center">
 
-    <div class="wrap">
+        <img class="button" id="button-previous" src="/images_app/back.svg"/>
 
-        <div id="agreements">
+        <div class="wrap">
 
-            @php $agreements = $evaluation->getAgreements @endphp
+            <div id="agreements">
 
-            @if($agreements->count() > 0)
+                @php $agreements = $evaluation->getAgreements @endphp
 
-                @foreach($agreements as $agreement)
+                @if($agreements->count() > 0)
 
-                    <div class="agreement" id="{{ $agreement->id }}" data-subject="{{ $agreement->subject }}" onclick="window.location.href='{{ route('agreement.view', ['identifier' => $agreement->{\App\Http\Support\Model::$AGREEMENT_IDENTIFIER}]) }}'">
+                    @foreach($agreements as $agreement)
 
-                        <div class="top">
+                        <div class="agreement" id="{{ $agreement->id }}" data-subject="{{ $agreement->subject }}" onclick="window.location.href='{{ route('agreement.view', ['identifier' => $agreement->{\App\Http\Support\Model::$AGREEMENT_IDENTIFIER}]) }}'">
 
-                            <div class="title">{{ $agreement->identifier }}</div>
+                            <div class="top">
+
+                                <div class="title">{{ $agreement->identifier }}</div>
+
+                            </div>
+
+                            <div class="bottom">
+
+                                @php $forHost = false @endphp
+
+                                @if($forHost ? $agreement->getStudent->getPerson->avatar : $agreement->getEmployee->getPerson->avatar)
+
+                                    <img src="{{ asset("storage/avatar/" . ($forHost ? $agreement->getStudent->getPerson->avatar : $agreement->getEmployee->getPerson->avatar)) }}"/>
+
+                                @else
+
+                                    <div>
+
+                                        <div class="no-avatar">{{ \App\Http\Traits\PersonTrait::getInitials($forHost ? $agreement->getStudent->getPerson : $agreement->getEmployee->getPerson) }}</div>
+
+                                    </div>
+
+                                @endif
+
+                                <div>{!! \App\Http\Traits\AgreementTrait::getDescription($agreement, $forHost) !!} @if(\App\Http\Traits\AgreementTrait::getStatus($agreement) == \App\Http\Traits\AgreementTrait::$STATUS_EXPIRED) <span class="expired"> (Verlopen)</span> @elseif($agreement->{\App\Http\Support\Model::$AGREEMENT_STATUS} == \App\Http\Traits\AgreementTrait::$STATUS_UNAPPROVED || $agreement->{\App\Http\Support\Model::$AGREEMENT_STATUS} == \App\Http\Traits\AgreementTrait::$STATUS_TRIAL) <span class="trial"> (Proefles)</span> @endif</div>
+
+                            </div>
 
                         </div>
 
-                        <div class="bottom">
+                    @endforeach
 
-                            @php $forHost = false @endphp
+                @else
 
-                            @if($forHost ? $agreement->getStudent->getPerson->avatar : $agreement->getEmployee->getPerson->avatar)
+                    <div class="block-note">Er zijn na aanleiding van dit gesprek geen nieuwe vakafspraken aangemaakt.</div>
 
-                                <img src="{{ asset("storage/avatar/" . ($forHost ? $agreement->getStudent->getPerson->avatar : $agreement->getEmployee->getPerson->avatar)) }}"/>
+                @endif
 
-                            @else
-
-                                <div>
-
-                                    <div class="no-avatar">{{ \App\Http\Traits\PersonTrait::getInitials($forHost ? $agreement->getStudent->getPerson : $agreement->getEmployee->getPerson) }}</div>
-
-                                </div>
-
-                            @endif
-
-                            <div>{!! \App\Http\Traits\AgreementTrait::getDescription($agreement, $forHost) !!} @if(\App\Http\Traits\AgreementTrait::getStatus($agreement) == \App\Http\Traits\AgreementTrait::$STATUS_EXPIRED) <span class="expired"> (Verlopen)</span> @elseif($agreement->{\App\Http\Support\Model::$AGREEMENT_STATUS} == \App\Http\Traits\AgreementTrait::$STATUS_UNAPPROVED || $agreement->{\App\Http\Support\Model::$AGREEMENT_STATUS} == \App\Http\Traits\AgreementTrait::$STATUS_TRIAL) <span class="trial"> (Proefles)</span> @endif</div>
-
-                        </div>
-
-                    </div>
-
-                @endforeach
-
-            @else
-
-                <div class="block-note">Er zijn na aanleiding van dit gesprek geen nieuwe vakafspraken aangemaakt.</div>
-
-            @endif
+            </div>
 
         </div>
 
-    </div>
+        <img class="button" id="button-next" src="/images_app/forward.svg"/>
 
-    <img class="button" id="button-next" src="/images_app/forward.svg"/>
+    </div>
 
 </div>
