e3d90929d2ff96455cfbc7b1a6ea5dfc1c6f7325
mano-gilissen <A7c20m1095>
2022-02-03 13:58:12 +0100

    LOAD MORE

--- resources/views/load/counters.blade.php
+++ resources/views/load/counters.blade.php
@@ -1,6 +1,6 @@
 @foreach($counters as $counter)
 
-    <div @if($counter->id) id="{{ $counter->id }}" @endif class="counter" @if($loop->first) style="margin-left:0" @endif>
+    <div @if($counter->id ?? false) id="{{ $counter->id }}" @endif class="counter" @if($loop->first) style="margin-left:0" @endif>
 
         <div class="label">{{ $counter->label }}</div>
 
