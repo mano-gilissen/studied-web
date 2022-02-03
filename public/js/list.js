b8be5882e6cc519f2aa261ca11ab839f6a5d7f74
mano-gilissen <A7c20m1095>
2022-02-03 14:04:00 +0100

    LOAD MORE

--- public/js/list.js
+++ public/js/list.js
@@ -202,8 +202,8 @@ function set_visibility_load_more() {
 
 function more_available() {
 
-    console.log(item_total());
-    console.log(item_count());
+    console.log('<br>it: ' + item_total());
+    console.log('<br>ic: ' + item_count());
 
     return item_total() >= 0 ? item_count() < item_total() : true;
 
