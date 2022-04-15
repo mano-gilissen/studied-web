7bf6b72d037d72115b2c156cecd3d1cd35e9f5dc
mano-gilissen <A7c20m1095>
2022-04-15 19:48:39 +0200

    RECAPTCHA

--- resources/views/website/werkwijze.blade.php
+++ resources/views/website/werkwijze.blade.php
@@ -16,6 +16,7 @@
     <link href="css/normalize.css" rel="stylesheet" type="text/css">
     <link href="css/webflow.css" rel="stylesheet" type="text/css">
     <link href="css/studied.webflow.css" rel="stylesheet" type="text/css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
     <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
@@ -974,6 +975,7 @@
         <div class="form-wrapper w-form">
             <form action="https://submit-form.com/xSgds62z" id="email-form" data-name="Email Form" autocomplete="off" class="cf-body"> <!-- name="email-form" -->
 
+                <div class="g-recaptcha" data-sitekey="6Lf_HAMdAAAAAAd_W2EK30A8Yz71TmZxacFIjkbH"></div>
                 <input type="hidden" name="_feedback.dark" value="false" />
                 <input type="hidden" name="_feedback.language" value="nl" />
                 <input type="hidden" name="_feedback.success.title" value="Je verzoek is verstuurd!" />
