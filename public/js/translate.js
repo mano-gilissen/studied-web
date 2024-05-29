18cfd9e69f477e53a797f8f3c1dd782f5a7c9893
manogilissen <mano.gilissen@gmail.com>
2024-05-29 10:50:36 +0800

    Translations

--- public/js/translate.js
+++ public/js/translate.js
@@ -20,17 +20,17 @@ function translateEn(value) {
     switch (value) {
 
         case "": return "";
-        "Vakafspraak bekijken"
-        "Profiel bekijken"
-        "Profiel van "
-        " bekijken"
-        "Sorteren"
-        "Filteren"
-        "Menu"
-        "Uitloggen"
-        "Instellingen"
-        "Kopiëren"
-        "Er is iets fout gegaan bij het wijzigen van de taal. Check je internetverbinding en probeer het nog eens."
+        case "Vakafspraak bekijken": return "View agreement";
+        case "Profiel bekijken": return "View profile";
+        case "View profile of ": return "Profile of ";
+        case " bekijken": return "";
+        case "Sorteren": return "Sort";
+        case "Filteren": return "Filter";
+        case "Menu": return "Menu";
+        case "Uitloggen": return "Log Out";
+        case "Instellingen": return "Settings";
+        case "Kopiëren": return "Copy";
+        case "Er is iets fout gegaan bij het wijzigen van de taal. Check je internetverbinding en probeer het nog eens.": return "Something went wrong while changing the language. Check your internet connection and try again.";
 
         default:
             return value;
