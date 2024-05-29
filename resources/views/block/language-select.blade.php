24c832257d17bdf9c51d6d300ce215f9c32f8075
manogilissen <mano.gilissen@gmail.com>
2024-05-29 09:42:29 +0800

    Translations

--- resources/views/block/language-select.blade.php
+++ resources/views/block/language-select.blade.php
@@ -4,13 +4,13 @@
 
     <div class="value">{{ \App\Http\Middleware\Locale::getActive_text() }}</div>
 
-    <img class="chevron" src="/images_app/chevron-down.svg"/>
+    <img class="chevron" src="/images_app/chevron-white-down.svg"/>
 
     <div class="options">
 
         @foreach(\App\Http\Middleware\Locale::getOptions() as $option)
 
-            <div class="option" onclick="set_language({{ $option }})">{{ \App\Http\Middleware\Locale::getOption_text($option) }}</div>
+            <div class="option" onclick="set_language('{{ $option }}')">{{ \App\Http\Middleware\Locale::getOption_text($option) }}</div>
 
         @endforeach
 
