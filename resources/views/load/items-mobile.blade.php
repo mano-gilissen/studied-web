6140cf87fcfccde03ce4eb7ea9e08f10a82afcec
manogilissen <mano.gilissen@gmail.com>
2025-05-26 19:59:33 +0800

    MOBILE LAYOUT

--- resources/views/load/items-mobile.blade.php
+++ resources/views/load/items-mobile.blade.php
@@ -2,6 +2,37 @@
 
     <div class="item" onclick="window.location.href='{{ $item->link ?? '' }}'">
 
+        @switch ($data_type)
+
+            @case(\App\Http\Support\Model::$STUDY)
+
+                @php
+
+                    $desiredOrder = [
+                        \App\Http\Controllers\StudyController::$COLUMN_DATE,
+                        \App\Http\Controllers\StudyController::$COLUMN_HOST,
+                        \App\Http\Controllers\StudyController::$COLUMN_TIME,
+                        \App\Http\Controllers\StudyController::$COLUMN_STUDENT,
+                        \App\Http\Controllers\StudyController::$COLUMN_SUBJECT,
+                        \App\Http\Controllers\StudyController::$COLUMN_SERVICE,
+                        \App\Http\Controllers\StudyController::$COLUMN_LOCATION,
+                        \App\Http\Controllers\StudyController::$COLUMN_STATUS,
+                    ];
+
+                    $sorted = $item->sortBy(function ($attribute) use ($desiredOrder) {
+                        return array_search($attribute->id, $desiredOrder);
+                    })->values();
+
+                @endphp
+
+                @break
+
+            @default
+
+                @break
+
+        @endswitch
+
         @foreach($columns as $column)
 
             <div class="attribute">
