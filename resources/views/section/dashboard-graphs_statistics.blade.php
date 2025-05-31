08708901a9ef59fa044fbab7cc6cb37dd7b6a3b9
manogilissen <mano.gilissen@gmail.com>
2025-05-31 19:15:41 +0800

    FILE RESTRUCTURE DASHBOARD DASHBOARD MODULE GRAPH

--- resources/views/section/dashboard-graphs_statistics.blade.php
+++ resources/views/section/dashboard-graphs_statistics.blade.php
@@ -12,9 +12,9 @@
 
             @endforeach
 
-            <div class="option" onclick="module_graphs_statistics__set_data('revenue', 'total')" style="margin-left: auto">{{ __('Totaal') }}</div>
+            <div class="option" onclick="module_graphs_statistics__set_data('revenue', false)" style="margin-left: auto">{{ __('Totaal') }}</div>
 
-            <div class="option selected" onclick="module_graphs_statistics__set_data('revenue', 'split')">{{ __('Per dienst') }}</div>
+            <div class="option selected" onclick="module_graphs_statistics__set_data('revenue', true)">{{ __('Per dienst') }}</div>
 
         </div>
 
@@ -38,9 +38,9 @@
 
             @endforeach
 
-            <div class="option" onclick="module_graphs_statistics__set_data('revenue', 'total')" style="margin-left: auto">{{ __('Totaal') }}</div>
+            <div class="option" onclick="module_graphs_statistics__set_data('revenue', false)" style="margin-left: auto">{{ __('Totaal') }}</div>
 
-            <div class="option selected" onclick="module_graphs_statistics__set_data('revenue', 'split')">{{ __('Per status') }}</div>
+            <div class="option selected" onclick="module_graphs_statistics__set_data('revenue', true)">{{ __('Per status') }}</div>
 
         </div>
 
