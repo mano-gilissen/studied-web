319a60d61d4df5afa7fddb9a1c160ee0d79564d8
manogilissen <mano.gilissen@gmail.com>
2025-06-04 17:45:06 +0800

    FILE RESTRUCTURE DASHBOARD DASHBOARD MODULE GRAPH

--- app/Http/Mail/Study_Planned_Employee.php
+++ app/Http/Mail/Study_Planned_Employee.php
@@ -41,7 +41,7 @@ class Study_Planned_Employee extends Mailable {
         $this->subject                              = __('Er is een :service voor je ingepland met :participants', ['service' => strtolower($study->getService->{Model::$SERVICE_SHORT}), 'participants' => StudyTrait::getParticipantsText($study)]);
 
         $this->invite                               = Func::generate_calendar_invite(
-            $study->{Model::$BASE_KEY} . time() . '@studied.com',
+            $study->{Model::$BASE_KEY} . time() . '@studied.nl',
             StudyTrait::getDescription($study),
             StudyTrait::getDescription($study),
             $study->{Model::$STUDY_LOCATION_TEXT},
