faa7b406d52cc7ca9dfba2548e95880f5c59da5a
manogilissen <mano.gilissen@gmail.com>
2025-06-05 00:41:54 +0800

    FILE RESTRUCTURE DASHBOARD DASHBOARD MODULE GRAPH

--- resources/views/component/todo.blade.php
+++ resources/views/component/todo.blade.php
@@ -1,15 +1,15 @@
-<div class="todo {{ $loop->odd ? 'odd' : '' }}" onclick="{!! $todo->link !!}">
+<div class="todo {{ $loop->odd ? 'odd' : '' }}" onclick="{!! $todo['link'] !!}">
 
-    <img class="priority" src="/images_app/dashboard-priority-{{ $todo->priority }}.svg">
+    <img class="priority" src="/images_app/dashboard-priority-{{ $todo['priority'] }}.svg">
 
     <div>
 
-        <div class="title">{{ $todo->title }}</div>
+        <div class="title">{{ $todo['title'] }}</div>
 
-        <div class="description">{{ $todo->description }}</div>
+        <div class="description">{{ $todo['description'] }}</div>
 
     </div>
 
-    <img class="action" src="/images_app/dashboard-action-{{ $todo->action }}.svg">
+    <img class="action" src="/images_app/dashboard-action-{{ $todo['action'] }}.svg">
 
 </div>
