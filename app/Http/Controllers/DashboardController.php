d3ee82b2620c820faa9d06c39b2378cb67529148
manogilissen <mano.gilissen@gmail.com>
2025-06-05 01:23:31 +0800

    FILE RESTRUCTURE DASHBOARD DASHBOARD MODULE GRAPH

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -271,7 +271,7 @@ class DashboardController extends Controller {
 
                 $description                                = __('Je loopt achter met het inplannen
                     van lessen voor de vakafspraak :subject met :student.
-                    Plan <span style="font-style: italic">:deficit uur</span> aan lessen extra in om deze achterstand in te halen.', [
+                    Plan <span>:deficit uur</span> aan lessen extra in om deze achterstand in te halen.', [
                     'student' => $student,
                     'subject' => $agreement->getSubject->{Model::$SUBJECT_NAME},
                     'deficit' => str_replace('.', ',', $deficit)
