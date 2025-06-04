b89df658578722abf7eff1d695946a2f075de19e
manogilissen <mano.gilissen@gmail.com>
2025-06-05 00:39:53 +0800

    FILE RESTRUCTURE DASHBOARD DASHBOARD MODULE GRAPH

--- app/Http/Support/Model.php
+++ app/Http/Support/Model.php
@@ -266,7 +266,8 @@ class Model {
         $REPORT_CONTENT_VOLGENDE_LES                    = 'content_volgende_les',
         $REPORT_CONTENT_UITDAGINGEN                     = 'content_uitdagingen',
         $REPORT_CONTENT_VOORTGANG                       = 'content_voortgang',
-        $REPORT_TRIAL_SUCCESS                           = 'trial_succes';
+        $REPORT_TRIAL_SUCCESS                           = 'trial_succes',
+        $REPORT_FLAGGED                                 = 'flagged';
 
 
 
