7aae6f933ba57a922c42163fe49fb867680514b5
manogilissen <mano.gilissen@gmail.com>
2025-07-09 18:42:29 +0200

    TRANSLATIONS

--- resources/views/mail/agreement_reminder_management.blade.php
+++ resources/views/mail/agreement_reminder_management.blade.php
@@ -6,9 +6,13 @@
         <td style="padding: 0 0 35px 0;">
             {{ __('Hi team,') }}
             <br><br>
-            {!! 'Vakafspraak ' . $agreement->identifier . ' (losse lessen) van '
-            . \App\Http\Traits\PersonTrait::getFullName($employee->getPerson) . ' heeft na 7 dagen nog geen lessen ingepland. '
-            . 'Klik <a href="' .  route('agreement.view', ['identifier' => $agreement->identifier]) . '">hier</a> om naar deze vakafspraak te gaan.' !!}
+            {!!
+                __('Vakafspraak :agreement (losse lessen) van :employee heeft na 7 dagen nog geen lessen ingepland. Klik <a href=":link">hier</a> om naar deze vakafspraak te gaan.', [
+                    'agreement' => $agreement->identifier,
+                    'employee' => \App\Http\Traits\PersonTrait::getFullName($employee->getPerson),
+                    'link' => route('agreement.view', ['identifier' => $agreement->identifier])
+                ])
+            !!}
             <br><br><br>
             {{ __('Groeten,') }}
             <br><br>
--- resources/views/section/dashboard-announcements.blade.php
+++ resources/views/section/dashboard-announcements.blade.php
@@ -31,7 +31,7 @@
                     <div class="author">
                         {!!
                             ($announcement->author ? ('<span style="font-weight:500">' . $announcement->author . '</span>&nbsp;&nbsp;') : '') .
-                            (\App\Http\Traits\BaseTrait::hasManagementRights() ? ('<span style="font-style:italic;opacity:.5">voor ' . \App\Http\Traits\RoleTrait::getName($announcement->role) . '</span>&nbsp;&nbsp;-&nbsp;&nbsp;') : ($announcement->author ? '-&nbsp;&nbsp;' : '')) .
+                            (\App\Http\Traits\BaseTrait::hasManagementRights() ? ('<span style="font-style:italic;opacity:.5">' . __('voor') . '&nbsp;' . \App\Http\Traits\RoleTrait::getName($announcement->role) . '</span>&nbsp;&nbsp;-&nbsp;&nbsp;') : ($announcement->author ? '-&nbsp;&nbsp;' : '')) .
                             \App\Http\Support\Format::datetime($announcement->created_at, \App\Http\Support\Format::$DATETIME_ANNOUNCEMENT)!!}
                     </div>
 
