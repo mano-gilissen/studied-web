f62edabf5b13d9b4fded0610a76622fefaaa527e
manogilissen <mano.gilissen@gmail.com>
2025-08-21 17:25:51 +0200

    TRANSLATIONS

--- resources/views/form/agreement-create.blade.php
+++ resources/views/form/agreement-create.blade.php
@@ -2,6 +2,36 @@
 
 
 
+@section('css-form')
+
+    <!-- Align subject and level fields vertically on mobile -->
+
+    <style>
+
+        @media (max-width: 840px) {
+
+            #form .block-form #field-subject-level  {
+                display: grid;
+                grid-template-columns: 1fr 2fr;
+                grid-template-rows: auto auto;
+                grid-row-gap: 32px;
+            }
+
+            #form .block-form #field-subject-level .width-third {
+                width: 100%;
+            }
+
+            #form .block-form #field-subject-level .width-third.name {
+                justify-content: start;
+            }
+        }
+
+    </style>
+
+@endsection
+
+
+
 @section('fields')
 
 
