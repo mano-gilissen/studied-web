53955559698abc8dbc2347528c52012cf92c1b37
manogilissen <mano.gilissen@gmail.com>
2025-10-09 20:54:42 +0200

    DEVELOPMENTS SUMMER 2025 LIVE

--- resources/views/section/dashboard-links.blade.php
+++ resources/views/section/dashboard-links.blade.php
@@ -2,34 +2,52 @@
 
     <div class="title">{{ __('Snelle links') }}</div>
 
-    <div class="link" onclick="window.open('https://www.studied.nl/studiemateriaal', '_blank')">
-
+    <div class="link" onclick="window.open('https://drive.google.com/drive/u/6/folders/13e_lUea8u92wHpAEf4-WdmqK4-hiZKjE', '_blank')">
         <img src="/images_app/website.svg" class="icon-link">
-
-        <div class="description">{{ __('Studiemateriaal') }}</div>
-
+        <div class="description">{{ __('Bibliotheek voor Onboarding- en Training') }}</div>
         <img src="/images_app/chevron-right.svg" class="icon-visit">
-
     </div>
 
-    <div class="link" onclick="window.open('https://www.studied.nl/maaltijdpakketten', '_blank')">
-
+    <div class="link" onclick="window.open('https://drive.google.com/drive/u/6/folders/1EbjaHLEZzIsw8otVzEAedhC-Ym5wbFFR', '_blank')">
         <img src="/images_app/website.svg" class="icon-link">
-
-        <div class="description">{{ __('Maaltijdpakketten') }}</div>
-
+        <div class="description">{{ __('Life Support Omgeving') }}</div>
         <img src="/images_app/chevron-right.svg" class="icon-visit">
-
     </div>
 
-    <div class="link" onclick="window.open('https://www.studied.nl/bas', '_blank')">
-
+    <div class="link" onclick="window.open('https://api.whatsapp.com/send/?phone=31613169929&text&type=phone_number&app_absent=0', '_blank')">
         <img src="/images_app/website.svg" class="icon-link">
-
-        <div class="description">{{ __('Chatten met Bas') }}</div>
-
+        <div class="description">{{ __('WhatsApp van Teamleider | Masha') }}</div>
         <img src="/images_app/chevron-right.svg" class="icon-visit">
+    </div>
 
+    <div class="link" onclick="window.open('https://api.whatsapp.com/send/?phone=31613769282&text&type=phone_number&app_absent=0', '_blank')">
+        <img src="/images_app/website.svg" class="icon-link">
+        <div class="description">{{ __('WhatsApp van HR | Sarah H') }}</div>
+        <img src="/images_app/chevron-right.svg" class="icon-visit">
+    </div>
+
+    <div class="link" onclick="window.open('https://api.whatsapp.com/send/?phone=31616018566&text&type=phone_number&app_absent=0', '_blank')">
+        <img src="/images_app/website.svg" class="icon-link">
+        <div class="description">{{ __('WhatsApp van Finance | Sarah E') }}</div>
+        <img src="/images_app/chevron-right.svg" class="icon-visit">
+    </div>
+
+    <div class="link" onclick="window.open('https://chat.whatsapp.com/IV1Sn4gE4sT3pTXy7CLEDn', '_blank')">
+        <img src="/images_app/website.svg" class="icon-link">
+        <div class="description">{{ __('WhatsApp groepschat voor Student-docenten') }}</div>
+        <img src="/images_app/chevron-right.svg" class="icon-visit">
+    </div>
+
+    <div class="link" onclick="window.open('https://docs.google.com/forms/d/e/1FAIpQLSe__Rth3ttStnsIjfKT9Xn86hK3UJIeCayxZS1bwOr3QGg6nA/viewform', '_blank')">
+        <img src="/images_app/website.svg" class="icon-link">
+        <div class="description">{{ __('Formulier voor Feedback') }}</div>
+        <img src="/images_app/chevron-right.svg" class="icon-visit">
+    </div>
+
+    <div class="link" onclick="window.open('https://docs.google.com/forms/d/e/1FAIpQLScMSRzKnbGCXgxuptXgHLuqFVsBT0gOJmzt44ArhqIyDiWqag/viewform?usp=dialog', '_blank')">
+        <img src="/images_app/website.svg" class="icon-link">
+        <div class="description">{{ __('Maandelijkse Update Formulier') }}</div>
+        <img src="/images_app/chevron-right.svg" class="icon-visit">
     </div>
 
 </div>
