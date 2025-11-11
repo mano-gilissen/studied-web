c94f1938ce78307ae5c94f857f51e128de297435
manogilissen <mano.gilissen@gmail.com>
2025-11-11 16:44:31 +0700

    Adding headoffice employees

--- resources/views/component/part-customer-edit.blade.php
+++ resources/views/component/part-customer-edit.blade.php
@@ -1,1 +1,3 @@
 @include('component.field-input', ['id' => 'category', 'tag' => __('Categorie') . '*', 'data' => true, 'icon' => 'search.svg', 'required' => true, 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'set_id' => old('_category') ?? $customer->{\App\Http\Support\Model::$USER_CATEGORY}])
+
+<div class="seperator"></div>
