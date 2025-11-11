0a6ed2c51ee4a16eebbda55e02d062d5c1666aa2
manogilissen <mano.gilissen@gmail.com>
2025-11-11 16:45:52 +0700

    Adding headoffice employees

--- resources/views/component/part-customer-edit.blade.php
+++ resources/views/component/part-customer-edit.blade.php
@@ -1,5 +1,5 @@
 <div class="title">{{ __('Organisatie') }}</div>
 
-@include('component.field-input', ['id' => 'category', 'tag' => __('Categorie') . '*', 'data' => true, 'icon' => 'search.svg', 'required' => true, 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'set_id' => old('_category') ?? $customer->{\App\Http\Support\Model::$USER_CATEGORY}])
+@include('component.field-input', ['id' => 'category', 'tag' => __('Categorie') . '*', 'data' => true, 'icon' => 'search.svg', 'required' => true, 'show_all' => true, 'reject_other' => true, 'uses_id' => true, 'set_id' => old('_category') ?? $customer->getUser->{\App\Http\Support\Model::$USER_CATEGORY}])
 
 <div class="seperator"></div>
